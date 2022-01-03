9ca2e6e274b67589ba601056e40d1e9c92f01324
Peter Hutchison <pjhutch17@talktalk.net>
2022-01-03 09:47:36 +0000

    Upload project files

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Tangled_Web_2019
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_Address = new System.Windows.Forms.ComboBox();
+            this.Load_WebPage = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.webBrowser2 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Address:";
+            // 
+            // txt_Address
+            // 
+            this.txt_Address.FormattingEnabled = true;
+            this.txt_Address.Location = new System.Drawing.Point(109, 9);
+            this.txt_Address.Name = "txt_Address";
+            this.txt_Address.Size = new System.Drawing.Size(478, 21);
+            this.txt_Address.TabIndex = 1;
+            this.txt_Address.TextUpdate += new System.EventHandler(this.txt_Address_TextUpdate);
+            this.txt_Address.Enter += new System.EventHandler(this.txt_Address_Enter);
+            // 
+            // Load_WebPage
+            // 
+            this.Load_WebPage.Location = new System.Drawing.Point(603, 7);
+            this.Load_WebPage.Name = "Load_WebPage";
+            this.Load_WebPage.Size = new System.Drawing.Size(50, 23);
+            this.Load_WebPage.TabIndex = 2;
+            this.Load_WebPage.Text = "Go";
+            this.Load_WebPage.UseVisualStyleBackColor = true;
+            this.Load_WebPage.Click += new System.EventHandler(this.Load_WebPage_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(15, 46);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1064, 625);
+            this.webBrowser1.TabIndex = 3;
+            this.webBrowser1.Url = new System.Uri("http://www.bing.com", System.UriKind.Absolute);
+            // 
+            // webBrowser2
+            // 
+            this.webBrowser2.Location = new System.Drawing.Point(276, 208);
+            this.webBrowser2.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser2.Name = "webBrowser2";
+            this.webBrowser2.Size = new System.Drawing.Size(250, 250);
+            this.webBrowser2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1090, 687);
+            this.Controls.Add(this.webBrowser2);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.Load_WebPage);
+            this.Controls.Add(this.txt_Address);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Tangled Web";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_Closing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox txt_Address;
+        private System.Windows.Forms.Button Load_WebPage;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.WebBrowser webBrowser2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,223 @@
+﻿/*
+ * Name: Tangled Web
+ * Author: P.J . Hutchison
+ * Date: 6 July 2019
+ * Conversion of VB version to C#
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tangled_Web_2019
+{
+    public partial class Form1 : Form
+    {
+        public string[] web_history;
+        public Int32 web_counter = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Open form, set first page and open history file.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            web_history = new string[80];
+            web_history[1] = "http://www.bing.com";
+            web_counter++;
+
+            Import_History_from_file();
+        }
+
+        /// <summary>
+        /// Update Address page
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txt_Address_Change(object sender, EventArgs e)
+        {
+            Load_WebPage_Click(sender, e);
+        }
+
+        /// <summary>
+        /// Save history to file when closing window.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Closing(object sender, FormClosingEventArgs e)
+        {
+            Export_History_to_file();
+        }
+        /// <summary>
+        /// Update web page when Address changed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txt_Address_TextUpdate(object sender, EventArgs e)
+        {
+            Load_WebPage_Click(sender, e);
+        }
+
+        /// <summary>
+        /// Resize webbrowser if form changed size
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            webBrowser1.Width = this.Width * 95 / 100;
+            webBrowser1.Height = this.Height * 85 / 100;
+        }
+
+        /// <summary>
+        /// Load web page from txt_address box and update history.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Load_WebPage_Click(object sender, EventArgs e)
+        {
+            string MyUrl;
+
+            MyUrl = txt_Address.Text;
+
+            if (MyUrl != "")
+            {
+                // Make sure url starts with HTTP protocol
+                if (MyUrl.StartsWith("http://") == false & MyUrl.StartsWith("https://") == false)
+                    MyUrl = "http://" + MyUrl;
+           
+                try
+                {
+                    webBrowser1.Navigate(new Uri(MyUrl, UriKind.RelativeOrAbsolute));
+                    Add_Url_to_History(MyUrl);
+                }
+
+                catch (SystemException sec) { 
+                    Console.WriteLine(sec.Message);
+                }
+                catch (Exception x) { 
+                    Console.WriteLine(x.Message);
+                }
+            
+            } // End if
+        }
+
+        /// <summary>
+        /// Add a web page url to history list
+        /// </summary>
+        /// <param name="url"></param>
+        void Add_Url_to_History (string url)
+        {
+            Boolean found = false;
+            string entry = "";
+            Int32 n;
+
+            try
+            {
+                // Check if url has not already been added
+                Console.WriteLine("Checking web history");
+                if (web_counter > 1)
+                {
+                    for (n = 1; n < web_counter; n++)
+                    {
+                        entry = web_history[n];
+                        Console.WriteLine(n.ToString() + " : " + entry);
+                        if (entry.Contains(url)) 
+                            found = true;                        
+                    }
+                }
+                Console.WriteLine("Found = " + found);
+                if (found == false)
+                {
+                    // Add page to history list
+                    web_history[web_counter] = url;
+                    Console.WriteLine("URL added to web history");
+                    web_counter = web_counter + 1;
+                    if (web_counter >= 80)
+                        web_counter = 1;
+                    
+                    // Add address to combo box
+                    txt_Address.Items.Add(url);
+                    Console.WriteLine("URL added to Combo box list");
+                }
+            }
+            catch ( Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Web_counter = " + web_counter);
+                Console.WriteLine("URL = " + url);
+            }
+        }
+
+        /// <summary>
+        /// Save history list to a file.
+        /// </summary>
+        void Export_History_to_file()
+        {
+            string  History_Path = "";
+            StreamWriter file1;
+
+            // History_Path = Microsoft.VisualBasic.Environ("USERPROFILE")
+            History_Path = Environment.GetEnvironmentVariable ("USERPROFILE");
+
+            //FileOpen(1, History_Path & "\History.txt", OpenMode.Append)
+            file1 = new StreamWriter(History_Path + "\\History.txt");
+            foreach (string urlEntry in txt_Address.Items)
+                file1.WriteLine(urlEntry);
+
+            //FileClose(1)
+            file1.Close();
+        }
+
+        /// <summary>
+        /// Import history from file to address list.
+        /// </summary>
+        void Import_History_from_file()
+        {
+            string urlEntry = "", History_Path = "";
+            StreamReader file1;
+
+            // History_Path = Microsoft.VisualBasic.Environ("USERPROFILE")
+            History_Path = Environment.GetEnvironmentVariable("USERPROFILE");
+
+            if (File.Exists(History_Path + "\\History.txt"))
+            {
+                // FileOpen(1, History_Path & "\History.txt", OpenMode.Input)
+                file1 = new StreamReader(History_Path + "\\History.txt");
+                while (! file1.EndOfStream)
+                {
+                    //Input(1, urlEntry)
+                    urlEntry = file1.ReadLine();
+                    txt_Address.Items.Add(urlEntry);
+                }
+                // FileClose(1)
+                file1.Close();
+            }
+        }
+
+        /// <summary>
+        /// Load new web page.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="e"></param>
+        void txt_Address_Enter(object s, EventArgs e)
+        {
+            Load_WebPage_Click(s, e);
+        }
+
+
+    }
+}
